22fc42490d22a672e1b31a6009ead76fc1946a40
ABDESSAMADE OUOMAR <119781541+ouomarabdessamade@users.noreply.github.com>
2023-08-17 01:21:51 +0100

    main.py

--- main.py
+++ main.py
@@ -452,16 +452,7 @@ def ndcg(gt_item, pred_items):
 	return 0
 
 
-def Recall_ATk(test_data, r, k):
-	"""
-    test_data should be a list? cause users may have different amount of pos items. shape (test_batch, k)
-    pred_data : shape (test_batch, k) NOTE: pred_data should be pre-sorted
-    k : top-k
-    """
-    # if all_pos_num == 0:
-    #     return 0
-	r = np.asfarray(r)[:k]
-	return np.sum(r) / len(test_data)
+
  
 
 
@@ -477,7 +468,7 @@ def MRRatK_r(r, k):
 	return np.sum(pred_data)
 
 
-def NDCGatK_r(test_data, r, k):
+def Recall_ATk(test_data, r, k):
 	"""
     test_data should be a list? cause users may have different amount of pos items. shape (test_batch, k)
     pred_data : shape (test_batch, k) NOTE: pred_data should be pre-sorted
@@ -490,30 +481,28 @@ def NDCGatK_r(test_data, r, k):
 	recall = np.sum(right_pred / recall_n)
 	
 	return recall
-"""
+
 def NDCGatK_r(test_data, r, k):
-    
+	"""
     Normalized Discounted Cumulative Gain
     rel_i = 1 or 0, so 2^{rel_i} - 1 = 1 or 0
+    """
+	assert len(r) == len(test_data)
+	pred_data = r[:, :k]
 
-    assert len(r) == len(test_data)
-    pred_data = r[:, :k]
+	test_matrix = np.zeros((len(pred_data), k))
+	for i, items in enumerate(test_data):
+		length = k if k <= len(items) else len(items)
+		test_matrix[i, :length] = 1
+	max_r = test_matrix
+	idcg = np.sum(max_r * 1. / np.log2(np.arange(2, k + 2)), axis=1)
+	dcg = pred_data * (1. / np.log2(np.arange(2, k + 2)))
+	dcg = np.sum(dcg, axis=1)
+	idcg[idcg == 0.] = 1.
+	ndcg = dcg / idcg
+	ndcg[np.isnan(ndcg)] = 0.
+	return np.sum(ndcg)
 
-    test_matrix = np.zeros((len(pred_data), k))
-    for i, items in enumerate(test_data):
-        length = k if k <= len(items) else len(items)
-        test_matrix[i, :length] = 1
-    max_r = test_matrix
-    idcg = np.sum(max_r * 1. / np.log2(np.arange(2, k + 2)), axis=1)
-    dcg = pred_data * (1. / np.log2(np.arange(2, k + 2)))
-    dcg = np.sum(dcg, axis=1)
-    idcg[idcg == 0.] = 1.
-    ndcg = dcg / idcg
-    ndcg[np.isnan(ndcg)] = 0.
-    
-    
-    return np.sum(ndcg)
-"""
 def test_one_batch(X, ks):
     recall_atk = []
     ncg_atk = []
